060a9d10a3d12fb2c7b3640016c1b435aa5d5103
Erich Grunewald <erichgrunewald@gmail.com>
2016-07-16 14:49:37 +0200

    Add two failing tests for edits function.

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -3,12 +3,42 @@ module Tests exposing (..)
 import String exposing (toList)
 import ElmTest exposing (..)
 
-import EditDistance exposing ( levenshtein )
+import EditDistance exposing ( Change (..), edits, levenshtein )
+
+-- Setup
+
+kitten : List Char
+kitten = (toList "kitten")
+
+sitting : List Char
+sitting = (toList "sitting")
+
+-- Tests
+
+edits_tests : List Test
+edits_tests =
+  [ test "kitten -> sitting edit steps" (
+      assertEqual (edits kitten sitting) (
+        [ Substitute 's' 0
+        , Substitute 'i' 4
+        , Insert 'g' 6
+        ]
+      )
+    )
+  , test "sitting -> kitten edit steps" (
+      assertEqual (edits sitting kitten) (
+        [ Substitute 'k' 0
+        , Substitute 'e' 4
+        , Delete 'g' 6
+        ]
+      )
+    )
+  ]
 
 levenshtein_tests : List Test
 levenshtein_tests =
-    [ test "kitten -> sitting levenshtein distance" (assertEqual (levenshtein (toList "kitten") (toList "sitting")) 3)
-    , test "sitting -> kitten levenshtein distance" (assertEqual (levenshtein (toList "sitting") (toList "kitten")) 3)
+    [ test "kitten -> sitting levenshtein distance" (assertEqual (levenshtein kitten sitting) 3)
+    , test "sitting -> kitten levenshtein distance" (assertEqual (levenshtein sitting kitten) 3)
     , test "preterit -> zeitgeist levenshtein distance" (assertEqual (levenshtein (toList "preterit") (toList "zeitgeist")) 6)
     , test "zeitgeist -> preterit levenshtein distance" (assertEqual (levenshtein (toList "zeitgeist") (toList "preterit")) 6)
     , test "garvey -> avery levenshtein distance" (assertEqual (levenshtein (toList "garvey") (toList "avery")) 3)
@@ -20,10 +50,11 @@ all_tests =
   let
     tests =
       List.concat
-        [ levenshtein_tests
+        [ edits_tests
+        , levenshtein_tests
         ]
   in
-    suite "Edit Distance tests" tests
+    suite "Edit Distance Tests" tests
 
 
 main : Program Never
