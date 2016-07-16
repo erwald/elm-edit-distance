a20a92bc85b4d3353abe4bc2eda4eff41728ccc5
Erich Grunewald <erichgrunewald@gmail.com>
2016-07-16 17:33:11 +0200

    Make the edits function support moves.

--- src/EditDistance.elm
+++ src/EditDistance.elm
@@ -1,36 +1,44 @@
-module EditDistance exposing ( Change (..), edits, levenshtein )
+module EditDistance exposing ( EditStep (..), edits, levenshtein )
 
 {-|  The EditDistance module allows for calculating the Levenshtein distance
 between two lists.
 
 # Edit Distance
-@docs Change, edits, levenshtein
+@docs EditStep, edits, levenshtein
 -}
 
 {-| Describes an edit step used to go from one list to another. -}
-type Change a
+type EditStep a
   = Insert a Int
   | Delete a Int
   | Substitute a Int
+  | Move a Int Int
 
-{-| Calculate the minimal steps (insertions, deletions and substitutions)
+type alias EditSteps a = List (EditStep a)
+
+{-| Calculate the minimal steps (insertions, deletions, moves and substitutions)
 required to turn one given list into another.
 
     edits (toList "kitten") (toList "sitting") ==
-      [ Substitute "r" 0
-      , Substitute "i" 4
-      , Insert "g" 0
+      [ Substitute 's' 0
+      , Substitute 'i' 4
+      , Insert 'g' 6
+      ]
+    edits (toList "garvey") (toList "avery") ==
+      [ Delete 'g' 0
+      , Move 'r' 2 3
       ]
 -}
-
-edits : List a -> List a -> List (Change a)
+edits : List a -> List a -> EditSteps a
 edits source target =
   let
     (result, _) = doEdits (List.reverse source) (List.reverse target)
   in
     List.reverse result
+      |> reduceMoves
 
-doEdits : List a -> List a -> (List (Change a), Int)
+{- Helper for edits function. -}
+doEdits : List a -> List a -> (EditSteps a, Int)
 doEdits source target =
   case (source, target) of
     ([], []) ->
@@ -74,6 +82,76 @@ doEdits source target =
             |> List.sortBy getCost
             |> List.head
             |> Maybe.withDefault ([], 0)
+
+{- Reduces a list of edit steps to combine insertions and deletions of the same
+value into a single Move action with that value. (These are equivalent anyway,
+as a deletion and insertion elsewhere of a certain value is nothing more than a
+movement of that value.)
+-}
+reduceMoves : EditSteps a -> EditSteps a
+reduceMoves editSteps =
+  let
+    findMove step acc =
+      case (moveFromSteps editSteps step) of
+        Just move -> acc ++ [move]
+        Nothing -> acc ++ [step]
+
+    {- Takes an element and a list and returns the list with the element added
+    if it wasn't already in the list. -}
+    addElementIfUnique elem list =
+      if (List.member elem list) then
+        list
+      else
+        list ++ [elem]
+
+    {- Takes a list and returns it with any duplicate elements removed. -}
+    dropDuplicates list =
+      List.foldl addElementIfUnique [] list
+  in
+    editSteps
+      |> List.foldl findMove []
+      |> dropDuplicates
+
+{- Takes an edit step and a list of edit steps and returns either a move step
+if there is one to be found for that edit step, or Nothing if not.
+-}
+moveFromSteps : EditSteps a -> EditStep a -> Maybe (EditStep a)
+moveFromSteps editSteps step =
+  case step of
+    Insert value index ->
+      let
+        {- Find the other corresponding deletion, if there is one. -}
+        isCorrespondingDelete step =
+          case step of
+            Delete otherValue _ -> value == otherValue
+            _ -> False
+        correspondingDelete =
+          editSteps
+            |> List.filter isCorrespondingDelete
+            |> List.head
+      in
+        case correspondingDelete of
+          Just (Delete _ deleteIndex) -> Just (Move value deleteIndex index)
+          _ -> Nothing
+
+    Delete value index ->
+      let
+        {- Find the other corresponding insertion, if there is one. -}
+        isCorrespondingInsert step =
+          case step of
+            Insert otherValue _ -> value == otherValue
+            _ -> False
+        correspondingInsert =
+          editSteps
+            |> List.filter isCorrespondingInsert
+            |> List.head
+      in
+        case correspondingInsert of
+          Just (Insert _ insertIndex) -> Just (Move value index insertIndex)
+          _ -> Nothing
+
+    _ ->
+      Nothing
 
 {-| Calculate the Levenshtein distance between two lists, i.e. how many
 insertions, deletions or substitutions are required to turn one given list into
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -3,7 +3,7 @@ module Tests exposing (..)
 import String exposing (toList)
 import ElmTest exposing (..)
 
-import EditDistance exposing ( Change (..), edits, levenshtein )
+import EditDistance exposing ( EditStep (..), edits, levenshtein )
 
 -- Setup
 
@@ -12,6 +12,12 @@ kitten = (toList "kitten")
 
 sitting : List Char
 sitting = (toList "sitting")
+
+garvey : List Char
+garvey = (toList "garvey")
+
+avery : List Char
+avery = (toList "avery")
 
 -- Tests
 
@@ -33,6 +39,20 @@ edits_tests =
         ]
       )
     )
+  , test "garvey -> avery edit steps" (
+      assertEqual (edits garvey avery) (
+        [ Delete 'g' 0
+        , Move 'r' 2 3
+        ]
+      )
+    )
+  , test "avery -> garvey edit steps" (
+      assertEqual (edits avery garvey) (
+        [ Insert 'g' 0
+        , Move 'r' 3 2
+        ]
+      )
+    )
   ]
 
 levenshtein_tests : List Test
@@ -41,8 +61,8 @@ levenshtein_tests =
     , test "sitting -> kitten levenshtein distance" (assertEqual (levenshtein sitting kitten) 3)
     , test "preterit -> zeitgeist levenshtein distance" (assertEqual (levenshtein (toList "preterit") (toList "zeitgeist")) 6)
     , test "zeitgeist -> preterit levenshtein distance" (assertEqual (levenshtein (toList "zeitgeist") (toList "preterit")) 6)
-    , test "garvey -> avery levenshtein distance" (assertEqual (levenshtein (toList "garvey") (toList "avery")) 3)
-    , test "avery -> garvey levenshtein distance" (assertEqual (levenshtein (toList "avery") (toList "garvey")) 3)
+    , test "garvey -> avery levenshtein distance" (assertEqual (levenshtein garvey avery) 3)
+    , test "avery -> garvey levenshtein distance" (assertEqual (levenshtein avery garvey) 3)
     ]
 
 all_tests : Test
