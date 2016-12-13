bd730e609c9407bc68942e5cca9545878abeba0b
Erich Grunewald <erichgrunewald@gmail.com>
2016-12-13 18:16:36 +0100

    Use elm-format.

--- src/EditDistance.elm
+++ src/EditDistance.elm
@@ -1,8 +1,15 @@
-module EditDistance exposing ( EditStep (..), edits, editsWithCostFunc,
-  editsFromStrings, editsWithCostFuncFromStrings, levenshtein,
-  levenshteinFromStrings )
+module EditDistance
+    exposing
+        ( EditStep(..)
+        , edits
+        , editsWithCostFunc
+        , editsFromStrings
+        , editsWithCostFuncFromStrings
+        , levenshtein
+        , levenshteinFromStrings
+        )
 
-{-|  The EditDistance module allows for calculating the Levenshtein distance
+{-| The EditDistance module allows for calculating the Levenshtein distance
 between two lists, or the actual edit steps required to go from one to the
 other.
 
@@ -16,13 +23,15 @@ other.
 
 import String exposing (toList)
 
+
 {-| Describes an edit step used to go from one list to another.
 -}
 type EditStep a
-  = Insert a Int
-  | Delete a Int
-  | Substitute a Int
-  | Move a Int Int
+    = Insert a Int
+    | Delete a Int
+    | Substitute a Int
+    | Move a Int Int
+
 
 {-| Calculate the minimum steps (insertions, deletions, moves and substitutions)
 required to turn one given list into another.
@@ -45,7 +54,8 @@ intermediate lists.
 -}
 edits : List comparable -> List comparable -> List (EditStep comparable)
 edits source target =
-  editsWithCostFunc (\_ -> 1) source target
+    editsWithCostFunc (\_ -> 1) source target
+
 
 {-| Same as `edits`, but also takes a custom cost function, which takes an
 EditStep (of type Insert, Delete or Substitute) and returns a cost (i.e. an
@@ -71,131 +81,165 @@ Int).
 converted into moves, meaning it will never receive an EditStep of type Move as
 an argument.)
 -}
-editsWithCostFunc : (EditStep comparable -> Int) -> List comparable -> List comparable -> List (EditStep comparable)
+editsWithCostFunc : (EditStep a -> Int) -> List comparable -> List comparable -> List (EditStep comparable)
 editsWithCostFunc costFunc source target =
-  let
-    (result, _) = doEdits costFunc (List.reverse source) (List.reverse target)
-  in
-    List.reverse result
-      |> reduceMoves
+    let
+        ( result, _ ) =
+            doEdits costFunc (List.reverse source) (List.reverse target)
+    in
+        List.reverse result
+            |> reduceMoves
 
-{-| Helper for edits function. -}
-doEdits : (EditStep comparable -> Int) -> List comparable -> List comparable -> (List (EditStep comparable), Int)
+
+{-| Helper for edits function.
+-}
+doEdits : (EditStep comparable -> Int) -> List comparable -> List comparable -> ( List (EditStep comparable), Int )
 doEdits costFunc source target =
-  case (source, target) of
-    ([], []) ->
-      ([], 0)
+    case ( source, target ) of
+        ( [], [] ) ->
+            ( [], 0 )
 
-    (src_hd::src_tail, []) ->
-      let
-        edit = Delete src_hd (List.length src_tail)
-        (result, cost) = doEdits costFunc src_tail []
-      in
-        (edit :: result, cost + (costFunc edit))
+        ( src_hd :: src_tail, [] ) ->
+            let
+                edit =
+                    Delete src_hd (List.length src_tail)
 
-    ([], tgt_hd::tgt_tail) ->
-      let
-        edit = Insert tgt_hd (List.length tgt_tail)
-        (result, cost) = doEdits costFunc [] tgt_tail
-      in
-        (edit :: result, cost + (costFunc edit))
+                ( result, cost ) =
+                    doEdits costFunc src_tail []
+            in
+                ( edit :: result, cost + (costFunc edit) )
 
-    (src_hd::src_tail, tgt_hd::tgt_tail) ->
-      if src_hd == tgt_hd then
-        doEdits costFunc src_tail tgt_tail
-      else
-        let
-          results =
-            [ doEdits costFunc src_tail (tgt_hd :: tgt_tail)
-            , doEdits costFunc (src_hd :: src_tail) tgt_tail
-            , doEdits costFunc src_tail tgt_tail
-            ]
-          edits =
-            [ Delete src_hd (List.length src_tail)
-            , Insert tgt_hd (List.length tgt_tail)
-            , Substitute tgt_hd (List.length tgt_tail)
-            ]
-          combineResultsWithEdits (res, cost) edit =
-            (edit :: res, cost + (costFunc edit))
-          getCost (_, cost) =
-            cost
-        in
-          List.map2 combineResultsWithEdits results edits
-            |> List.sortBy getCost
-            |> List.head
-            |> Maybe.withDefault ([], 0)
+        ( [], tgt_hd :: tgt_tail ) ->
+            let
+                edit =
+                    Insert tgt_hd (List.length tgt_tail)
+
+                ( result, cost ) =
+                    doEdits costFunc [] tgt_tail
+            in
+                ( edit :: result, cost + (costFunc edit) )
+
+        ( src_hd :: src_tail, tgt_hd :: tgt_tail ) ->
+            if src_hd == tgt_hd then
+                doEdits costFunc src_tail tgt_tail
+            else
+                let
+                    results =
+                        [ doEdits costFunc src_tail (tgt_hd :: tgt_tail)
+                        , doEdits costFunc (src_hd :: src_tail) tgt_tail
+                        , doEdits costFunc src_tail tgt_tail
+                        ]
+
+                    edits =
+                        [ Delete src_hd (List.length src_tail)
+                        , Insert tgt_hd (List.length tgt_tail)
+                        , Substitute tgt_hd (List.length tgt_tail)
+                        ]
+
+                    combineResultsWithEdits ( res, cost ) edit =
+                        ( edit :: res, cost + (costFunc edit) )
+
+                    getCost ( _, cost ) =
+                        cost
+                in
+                    List.map2 combineResultsWithEdits results edits
+                        |> List.sortBy getCost
+                        |> List.head
+                        |> Maybe.withDefault ( [], 0 )
+
+
 
 {- Reduces a list of edit steps to combine insertions and deletions of the same
-value into a single Move action with that value. (These are equivalent anyway,
-as a deletion and insertion elsewhere of a certain value is nothing more than a
-movement of that value.)
+   value into a single Move action with that value. (These are equivalent anyway,
+   as a deletion and insertion elsewhere of a certain value is nothing more than a
+   movement of that value.)
 -}
+
+
 reduceMoves : List (EditStep comparable) -> List (EditStep comparable)
 reduceMoves editSteps =
-  let
-    findMove step acc =
-      case (moveFromSteps editSteps step) of
-        Just move -> acc ++ [move]
-        Nothing -> acc ++ [step]
+    let
+        findMove step acc =
+            case (moveFromSteps editSteps step) of
+                Just move ->
+                    acc ++ [ move ]
 
-    {- Takes an element and a list and returns the list with the element added
-    if it wasn't already in the list.
-    -}
-    addElementIfUnique elem list =
-      if (List.member elem list) then
-        list
-      else
-        list ++ [elem]
+                Nothing ->
+                    acc ++ [ step ]
 
-    {- Takes a list and returns it with any duplicate elements removed.
-    -}
-    dropDuplicates list =
-      List.foldl addElementIfUnique [] list
-  in
-    editSteps
-      |> List.foldl findMove []
-      |> dropDuplicates
+        {- Takes an element and a list and returns the list with the element added
+           if it wasn't already in the list.
+        -}
+        addElementIfUnique elem list =
+            if (List.member elem list) then
+                list
+            else
+                list ++ [ elem ]
+
+        {- Takes a list and returns it with any duplicate elements removed. -}
+        dropDuplicates list =
+            List.foldl addElementIfUnique [] list
+    in
+        editSteps
+            |> List.foldl findMove []
+            |> dropDuplicates
+
 
 {-| Takes an edit step and a list of edit steps and returns either a move step
 if there is one to be found for that edit step, or Nothing if not.
 -}
 moveFromSteps : List (EditStep comparable) -> EditStep comparable -> Maybe (EditStep comparable)
 moveFromSteps editSteps step =
-  case step of
-    Insert value index ->
-      let
-        -- Find the corresponding deletion, if there is one.
-        isCorrespondingDelete step =
-          case step of
-            Delete otherValue _ -> value == otherValue
-            _ -> False
-        correspondingDelete =
-          editSteps
-            |> List.filter isCorrespondingDelete
-            |> List.head
-      in
-        case correspondingDelete of
-          Just (Delete _ deleteIndex) -> Just (Move value deleteIndex index)
-          _ -> Nothing
+    case step of
+        Insert value index ->
+            let
+                -- Find the corresponding deletion, if there is one.
+                isCorrespondingDelete step =
+                    case step of
+                        Delete otherValue _ ->
+                            value == otherValue
 
-    Delete value index ->
-      let
-        -- Find the corresponding insertion, if there is one.
-        isCorrespondingInsert step =
-          case step of
-            Insert otherValue _ -> value == otherValue
-            _ -> False
-        correspondingInsert =
-          editSteps
-            |> List.filter isCorrespondingInsert
-            |> List.head
-      in
-        case correspondingInsert of
-          Just (Insert _ insertIndex) -> Just (Move value index insertIndex)
-          _ -> Nothing
+                        _ ->
+                            False
 
-    _ ->
-      Nothing
+                correspondingDelete =
+                    editSteps
+                        |> List.filter isCorrespondingDelete
+                        |> List.head
+            in
+                case correspondingDelete of
+                    Just (Delete _ deleteIndex) ->
+                        Just (Move value deleteIndex index)
+
+                    _ ->
+                        Nothing
+
+        Delete value index ->
+            let
+                -- Find the corresponding insertion, if there is one.
+                isCorrespondingInsert step =
+                    case step of
+                        Insert otherValue _ ->
+                            value == otherValue
+
+                        _ ->
+                            False
+
+                correspondingInsert =
+                    editSteps
+                        |> List.filter isCorrespondingInsert
+                        |> List.head
+            in
+                case correspondingInsert of
+                    Just (Insert _ insertIndex) ->
+                        Just (Move value index insertIndex)
+
+                    _ ->
+                        Nothing
+
+        _ ->
+            Nothing
+
 
 {-| Same as the `edits` function, but for String values.
 
@@ -212,7 +256,8 @@ moveFromSteps editSteps step =
 -}
 editsFromStrings : String -> String -> List (EditStep Char)
 editsFromStrings source target =
-  edits (String.toList source) (String.toList target)
+    edits (String.toList source) (String.toList target)
+
 
 {-| Same as the `editsWithCostFunc` function, but for String values.
 
@@ -234,7 +279,8 @@ editsFromStrings source target =
 -}
 editsWithCostFuncFromStrings : (EditStep Char -> Int) -> String -> String -> List (EditStep Char)
 editsWithCostFuncFromStrings costFunc source target =
-  editsWithCostFunc costFunc (String.toList source) (String.toList target)
+    editsWithCostFunc costFunc (String.toList source) (String.toList target)
+
 
 {-| Calculate the Levenshtein distance between two lists, i.e. how many
 insertions, deletions or substitutions are required to turn one given list
@@ -251,22 +297,25 @@ into another.
 -}
 levenshtein : List comparable -> List comparable -> Int
 levenshtein source target =
-  case (source, target) of
-    (source, []) ->
-      List.length source
+    case ( source, target ) of
+        ( source, [] ) ->
+            List.length source
 
-    ([], target) ->
-      List.length target
+        ( [], target ) ->
+            List.length target
 
-    (src_hd::src_tail, tgt_hd::tgt_tail) ->
-      if src_hd == tgt_hd then
-        levenshtein src_tail tgt_tail
-      else
-        Maybe.withDefault 0 (List.minimum
-          [ (levenshtein src_tail target) + 1
-          , (levenshtein source tgt_tail) + 1
-          , (levenshtein src_tail tgt_tail) + 1
-          ])
+        ( src_hd :: src_tail, tgt_hd :: tgt_tail ) ->
+            if src_hd == tgt_hd then
+                levenshtein src_tail tgt_tail
+            else
+                Maybe.withDefault 0
+                    (List.minimum
+                        [ (levenshtein src_tail target) + 1
+                        , (levenshtein source tgt_tail) + 1
+                        , (levenshtein src_tail tgt_tail) + 1
+                        ]
+                    )
+
 
 {-| Same as the `levenshtein` function, but for String values.
 
@@ -278,4 +327,4 @@ levenshtein source target =
 -}
 levenshteinFromStrings : String -> String -> Int
 levenshteinFromStrings source target =
-  levenshtein (String.toList source) (String.toList target)
+    levenshtein (String.toList source) (String.toList target)
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,188 +2,222 @@ module Tests exposing (..)
 
 import String exposing (toList)
 import ElmTest exposing (..)
+import EditDistance
+    exposing
+        ( EditStep(..)
+        , edits
+        , editsWithCostFunc
+        , editsFromStrings
+        , levenshtein
+        , levenshteinFromStrings
+        )
 
-import EditDistance exposing ( EditStep (..), edits, editsWithCostFunc,
-  editsFromStrings, levenshtein, levenshteinFromStrings )
 
 -- Setup
 
+
 kitten : List Char
-kitten = (toList "kitten")
+kitten =
+    (toList "kitten")
+
 
 sitting : List Char
-sitting = (toList "sitting")
+sitting =
+    (toList "sitting")
+
 
 garvey : List Char
-garvey = (toList "garvey")
+garvey =
+    (toList "garvey")
+
 
 avery : List Char
-avery = (toList "avery")
+avery =
+    (toList "avery")
+
 
 preterit : List Char
-preterit = (toList "preterit")
+preterit =
+    (toList "preterit")
+
 
 zeitgeist : List Char
-zeitgeist = (toList "zeitgeist")
+zeitgeist =
+    (toList "zeitgeist")
+
 
 abc : List Char
-abc = (toList "abc")
+abc =
+    (toList "abc")
+
 
 adc : List Char
-adc = (toList "adc")
+adc =
+    (toList "adc")
+
+
 
 -- Tests for the edits functions.
 
+
 editsTests : List Test
 editsTests =
-  [ test "kitten -> sitting edit steps" (
-      assertEqual (edits kitten sitting) (
-        [ Substitute 's' 0
-        , Substitute 'i' 4
-        , Insert 'g' 6
-        ]
-      )
-    )
-  , test "sitting -> kitten edit steps" (
-      assertEqual (edits sitting kitten) (
-        [ Substitute 'k' 0
-        , Substitute 'e' 4
-        , Delete 'g' 6
-        ]
-      )
-    )
-  , test "garvey -> avery edit steps" (
-      assertEqual (edits garvey avery) (
-        [ Delete 'g' 0
-        , Move 'r' 2 3
-        ]
-      )
-    )
-  , test "avery -> garvey edit steps" (
-      assertEqual (edits avery garvey) (
-        [ Insert 'g' 0
-        , Move 'r' 3 2
-        ]
-      )
-    )
-  , test "preterit -> zeitgeist edit steps" (
-      assertEqual (edits preterit zeitgeist) (
-        [ Substitute 'z' 0
-        , Delete 'r' 1
-        , Insert 'i' 2
-        , Insert 'g' 4
-        , Delete 'r' 5
-        , Insert 's' 7
-        ]
-      )
-    )
-  , test "zeitgeist -> preterit edit steps" (
-      assertEqual (edits zeitgeist preterit) (
-        [ Substitute 'p' 0
-        , Insert 'r' 1
-        , Delete 'i' 2
-        , Delete 'g' 4
-        , Insert 'r' 5
-        , Delete 's' 7
-        ]
-      )
-    )
-  , test "kitten -> empty string edit steps" (
-      assertEqual (edits kitten []) (
-        [ Delete 'k' 0
-        , Delete 'i' 1
-        , Delete 't' 2
-        , Delete 't' 3
-        , Delete 'e' 4
-        , Delete 'n' 5
-        ]
-      )
-    )
-  , test "empty string -> kitten edit steps" (
-      assertEqual (edits [] kitten) (
-        [ Insert 'k' 0
-        , Insert 'i' 1
-        , Insert 't' 2
-        , Insert 't' 3
-        , Insert 'e' 4
-        , Insert 'n' 5
-        ]
-      )
-    )
-  ]
+    [ test "kitten -> sitting edit steps"
+        (assertEqual (edits kitten sitting)
+            ([ Substitute 's' 0
+             , Substitute 'i' 4
+             , Insert 'g' 6
+             ]
+            )
+        )
+    , test "sitting -> kitten edit steps"
+        (assertEqual (edits sitting kitten)
+            ([ Substitute 'k' 0
+             , Substitute 'e' 4
+             , Delete 'g' 6
+             ]
+            )
+        )
+    , test "garvey -> avery edit steps"
+        (assertEqual (edits garvey avery)
+            ([ Delete 'g' 0
+             , Move 'r' 2 3
+             ]
+            )
+        )
+    , test "avery -> garvey edit steps"
+        (assertEqual (edits avery garvey)
+            ([ Insert 'g' 0
+             , Move 'r' 3 2
+             ]
+            )
+        )
+    , test "preterit -> zeitgeist edit steps"
+        (assertEqual (edits preterit zeitgeist)
+            ([ Substitute 'z' 0
+             , Delete 'r' 1
+             , Insert 'i' 2
+             , Insert 'g' 4
+             , Delete 'r' 5
+             , Insert 's' 7
+             ]
+            )
+        )
+    , test "zeitgeist -> preterit edit steps"
+        (assertEqual (edits zeitgeist preterit)
+            ([ Substitute 'p' 0
+             , Insert 'r' 1
+             , Delete 'i' 2
+             , Delete 'g' 4
+             , Insert 'r' 5
+             , Delete 's' 7
+             ]
+            )
+        )
+    , test "kitten -> empty string edit steps"
+        (assertEqual (edits kitten [])
+            ([ Delete 'k' 0
+             , Delete 'i' 1
+             , Delete 't' 2
+             , Delete 't' 3
+             , Delete 'e' 4
+             , Delete 'n' 5
+             ]
+            )
+        )
+    , test "empty string -> kitten edit steps"
+        (assertEqual (edits [] kitten)
+            ([ Insert 'k' 0
+             , Insert 'i' 1
+             , Insert 't' 2
+             , Insert 't' 3
+             , Insert 'e' 4
+             , Insert 'n' 5
+             ]
+            )
+        )
+    ]
+
 
 editsWithCostFuncTests : List Test
 editsWithCostFuncTests =
-  let
-    costFunc editStep =
-      case editStep of
-        Substitute _ _ -> 3
-        _ -> 1
-  in
-    [ test "abc -> adc (without cost function) edit steps" (
-        assertEqual (edits abc adc) (
-          [ Substitute 'd' 1
-          ]
-        )
-      )
-    , test "abc ~> adc cost function edit steps" (
-        assertEqual (editsWithCostFunc costFunc abc adc) (
-          [ Insert 'd' 1
-          , Delete 'b' 1
-          ]
-        )
-      )
-    ]
+    let
+        costFunc editStep =
+            case editStep of
+                Substitute _ _ ->
+                    3
+
+                _ ->
+                    1
+    in
+        [ test "abc -> adc (without cost function) edit steps"
+            (assertEqual (edits abc adc)
+                ([ Substitute 'd' 1
+                 ]
+                )
+            )
+        , test "abc ~> adc cost function edit steps"
+            (assertEqual (editsWithCostFunc costFunc abc adc)
+                ([ Insert 'd' 1
+                 , Delete 'b' 1
+                 ]
+                )
+            )
+        ]
+
 
 editsFromStringsTests : List Test
 editsFromStringsTests =
-  [ test "preterit -> zeitgeist (as String values) edit steps" (
-      assertEqual (editsFromStrings "preterit" "zeitgeist") (
-        [ Substitute 'z' 0
-        , Delete 'r' 1
-        , Insert 'i' 2
-        , Insert 'g' 4
-        , Delete 'r' 5
-        , Insert 's' 7
-        ]
-      )
-    )
-  , test "zeitgeist -> preterit (as String values) edit steps" (
-      assertEqual (editsFromStrings "zeitgeist" "preterit") (
-        [ Substitute 'p' 0
-        , Insert 'r' 1
-        , Delete 'i' 2
-        , Delete 'g' 4
-        , Insert 'r' 5
-        , Delete 's' 7
-        ]
-      )
-    )
-  , test "kitten -> empty string (as String values) edit steps" (
-      assertEqual (editsFromStrings "kitten" "") (
-        [ Delete 'k' 0
-        , Delete 'i' 1
-        , Delete 't' 2
-        , Delete 't' 3
-        , Delete 'e' 4
-        , Delete 'n' 5
-        ]
-      )
-    )
-  , test "empty string -> kitten (as String values) edit steps" (
-      assertEqual (editsFromStrings "" "kitten") (
-        [ Insert 'k' 0
-        , Insert 'i' 1
-        , Insert 't' 2
-        , Insert 't' 3
-        , Insert 'e' 4
-        , Insert 'n' 5
-        ]
-      )
-    )
-  ]
+    [ test "preterit -> zeitgeist (as String values) edit steps"
+        (assertEqual (editsFromStrings "preterit" "zeitgeist")
+            ([ Substitute 'z' 0
+             , Delete 'r' 1
+             , Insert 'i' 2
+             , Insert 'g' 4
+             , Delete 'r' 5
+             , Insert 's' 7
+             ]
+            )
+        )
+    , test "zeitgeist -> preterit (as String values) edit steps"
+        (assertEqual (editsFromStrings "zeitgeist" "preterit")
+            ([ Substitute 'p' 0
+             , Insert 'r' 1
+             , Delete 'i' 2
+             , Delete 'g' 4
+             , Insert 'r' 5
+             , Delete 's' 7
+             ]
+            )
+        )
+    , test "kitten -> empty string (as String values) edit steps"
+        (assertEqual (editsFromStrings "kitten" "")
+            ([ Delete 'k' 0
+             , Delete 'i' 1
+             , Delete 't' 2
+             , Delete 't' 3
+             , Delete 'e' 4
+             , Delete 'n' 5
+             ]
+            )
+        )
+    , test "empty string -> kitten (as String values) edit steps"
+        (assertEqual (editsFromStrings "" "kitten")
+            ([ Insert 'k' 0
+             , Insert 'i' 1
+             , Insert 't' 2
+             , Insert 't' 3
+             , Insert 'e' 4
+             , Insert 'n' 5
+             ]
+            )
+        )
+    ]
+
+
 
 -- Tests for the levenshtein functions.
+
 
 levenshteinTests : List Test
 levenshteinTests =
@@ -197,6 +231,7 @@ levenshteinTests =
     , test "empty string <-> kitten levenshtein distance" (assertEqual (levenshtein [] kitten) 6)
     ]
 
+
 levenshteinFromStringsTests : List Test
 levenshteinFromStringsTests =
     [ test "kitten <-> sitting (as String values) levenshtein distance" (assertEqual (levenshteinFromStrings "kitten" "sitting") 3)
@@ -205,23 +240,26 @@ levenshteinFromStringsTests =
     , test "empty string <-> kitten (as String values) levenshtein distance" (assertEqual (levenshteinFromStrings "" "kitten") 6)
     ]
 
+
+
 -- Test execution.
+
 
 allTests : Test
 allTests =
-  let
-    tests =
-      List.concat
-        [ editsTests
-        , editsWithCostFuncTests
-        , editsFromStringsTests
-        , levenshteinTests
-        , levenshteinFromStringsTests
-        ]
-  in
-    suite "Edit Distance Tests" tests
+    let
+        tests =
+            List.concat
+                [ editsTests
+                , editsWithCostFuncTests
+                , editsFromStringsTests
+                , levenshteinTests
+                , levenshteinFromStringsTests
+                ]
+    in
+        suite "Edit Distance Tests" tests
 
 
 main : Program Never
 main =
-  runSuite allTests
+    runSuite allTests
